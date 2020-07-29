5c7281d5beb4c45dd12ad45568cd48fd896f4ee3
Tmuat <Thomas_Muat@hotmail.com>
2020-07-29 13:36:55 +0000

    Lots of minor tweaks to scripts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,19 +1,19 @@
-$('#carousel-example').on('slide.bs.carousel', function (e) {
-
+jQuery(document).ready(function () {
+  $("#carousel-work").on("slide.bs.carousel", function (e) {
     var $e = $(e.relatedTarget);
     var idx = $e.index();
-    var itemsPerSlide = 5;
-    var totalItems = $('.carousel-item').length;
- 
-    if (idx >= totalItems-(itemsPerSlide-1)) {
-        var it = itemsPerSlide - (totalItems - idx);
-        for (var i=0; i<it; i++) {
-            if (e.direction=="left") {
-                $('.carousel-item').eq(i).appendTo('.carousel-inner');
-            }
-            else {
-                $('.carousel-item').eq(0).appendTo('.carousel-inner');
-            }
+    var itemsPerSlide = 3;
+    var totalItems = $(".carousel-item").length;
+
+    if (idx >= totalItems - (itemsPerSlide - 1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i = 0; i < it; i++) {
+        if (e.direction == "left") {
+          $(".carousel-item").eq(i).appendTo(".carousel-inner");
+        } else {
+          $(".carousel-item").eq(0).appendTo(".carousel-inner");
         }
+      }
     }
+  });
 });
